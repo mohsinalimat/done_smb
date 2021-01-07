6f879ce1c39aa0e036d2d33de4d476ca0733af0d
venkat102 <venkatesharunachalam659@gmail.com>
2021-01-07 16:48:12 +0530

    feat[custody]: add custody doctype feat[salary certificate]: add salary certificate feat[print format]: add salary certificate print format feat[print format]: add employee resignation print format

---
+++ done_smb/done_smb/doctype/salary_certificate/salary_certificate.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, smb and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Salary Certificate', {
+	// refresh: function(frm) {
+
+	// }
+});
