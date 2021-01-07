5cb0647646dbceae59ea0ab344318a27ed2ed6ae
venkat102 <venkatesharunachalam659@gmail.com>
2021-01-07 17:58:30 +0530

    feat[print format]: add to whom it concern

--- done_smb/done_smb/doctype/salary_certificate/salary_certificate.js
+++ done_smb/done_smb/doctype/salary_certificate/salary_certificate.js
@@ -2,7 +2,21 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Salary Certificate', {
-	// refresh: function(frm) {
+	onload:function(frm){
+		console.log("loaded")
+		frappe.call({
+			method: 'erpnext.hr.doctype.salary_certificate.salary_certificate.getuser',
+			args: {
+				'user':frappe.session.user_email,
+			},
+			callback:function(res){
+				if(!res.exc){
+					console.log(res.message)
+					frm.set_value('employee_id', res.message[0].name);
+					frm.set_value('employee_name', res.message[0].employee_name);
 
-	// }
+				}
+			}
+		});
+	},
 });
--- done_smb/done_smb/doctype/salary_certificate/salary_certificate.py
+++ done_smb/done_smb/doctype/salary_certificate/salary_certificate.py
@@ -3,8 +3,13 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
+
+@frappe.whitelist()
+def getuser(user):
+	emp = frappe.db.get_list("Employee", filters={'user_id':['=',user]}, fields=['name','employee_name'])
+	return emp
 
 class SalaryCertificate(Document):
 	pass
