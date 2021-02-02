822c01a3554d48aed461dc1b8475238fd862d722
kavin <kavinkumarnkm007@gmail.com>
2021-02-02 18:58:08 +0530

    fix[coc] : fix multiple item error

--- done_smb/done_smb/doctype/custom_order_contract/custom_order_contract.py
+++ done_smb/done_smb/doctype/custom_order_contract/custom_order_contract.py
@@ -44,7 +44,7 @@ class CustomOrderContract(Document):
 			wo.production_item = self.item[i].item
 			wo.qty = int(self.item[i].qty)
 			# if frappe.db.exists("BOM",{"item":  self.item[i].item,"is_default":1},['name']):
-			wo.bom_no = frappe.db.get_value("BOM",{"item":  self.item[i].item,"is_default":1},['name'])
+			wo.bom_no = frappe.db.get_value("BOM",{"item":  self.item[i].item, "docstatus":1},['name'])
 			wo.wip_warehouse = "All Warehouses - DI"
 			wo.fg_warehouse = "All Warehouses - DI"
 			wo.sales_order = sales_no
