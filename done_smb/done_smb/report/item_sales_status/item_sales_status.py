f81b05bd7308a1f922e3685e06dd6cb46c0382d6
venkat102 <venkatesharunachalam659@gmail.com>
2021-02-03 19:44:21 +0530

    feat[item_sales_status]: add item sales status report delete: moving item status

--- done_smb/done_smb/report/item_sales_status/item_sales_status.py
+++ done_smb/done_smb/report/item_sales_status/item_sales_status.py
@@ -11,43 +11,39 @@ def execute(filters=None):
 			"fieldname": "item_code",
 			"fieldtype": "Link",
 			"label": "Item Code",
-			"width": 0,
+			"width": 150,
 			"options": "Item"
 		},
 		{
-			"fieldname": "item_name",
-			"fieldtype": "Data",
-			"label": "Item Name",
-			"width": 0
+		 	"fieldname": "item_name",
+		 	"fieldtype": "Data",
+		 	"label": "Item Name",
+		 	"width": 150
 		},
 		 {
 		 	"fieldname": "item_group",
 		 	"fieldtype": "Data",
 		 	"label": "Item Group",
-		 	"width": 0
+		 	"width": 150
 		},
 		{
 			"fieldname": "uom",
 			"fieldtype": "data",
 			"label": "UOM",
-			"width": 0
+			"width": 150
 		},
 		{
 			"fieldname": "qty",
 			"fieldtype": "Data",
-			"label": "Quantity",
-			"width": 0
-		},
-		
+			"label": "Quantity Sold",
+			"width": 150
+		}		
 		
 	]
 	if filters.from_date and filters.to_date:
-		print(filters)
 		data = get_data(filters.from_date, filters.to_date)
 	return columns, data
 
 def get_data(from_date, to_date):
-	print(from_date, to_date)
 	data = frappe.db.sql(''' select sii.item_code, sii.item_name, sii.item_group, sii.stock_uom, sum(sii.stock_qty) from `tabSales Invoice Item` as sii inner join `tabSales Invoice` as si on si.name = sii.parent where si.posting_date >= %s and si.posting_date <= %s group by  sii.item_code, sii.item_name, sii.item_group, sii.stock_uom order by sum(sii.stock_qty) DESC''',(from_date, to_date),as_list =1)
-	print("~~~~~~~~~~~~~~",data)
 	return data
